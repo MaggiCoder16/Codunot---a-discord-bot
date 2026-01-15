cabe63c8ef9b95be2ae824933ebdbf96aeae872b
maggicoder <156202777+MaggiCoder16@users.noreply.github.com>
2026-01-15 11:15:44 +0530

    Update deAPI_client_image.py

--- deAPI_client_image.py
+++ deAPI_client_image.py
@@ -14,7 +14,7 @@ DEAPI_API_KEY = os.getenv("DEAPI_API_KEY")
 if not DEAPI_API_KEY:
     raise RuntimeError("DEAPI_API_KEY not set")
 
-MODEL_NAME = "Flux.1 schnell"  # Make sure this matches your deAPI account
+MODEL_NAME = "Flux1schnell"  # Make sure this matches your deAPI account
 print(f"🔥 USING deAPI model {MODEL_NAME} 🔥")
 
 # ============================================================
